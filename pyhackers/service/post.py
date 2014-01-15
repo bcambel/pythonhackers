3d0e9147378896c088f0b9add0890c641c33c3f0
Bahadir Cambel <bcambel@gmail.com>
2014-01-15 16:45:05 +0100

    refactor messaging for discussions

--- pyhackers/service/post.py
+++ pyhackers/service/post.py
@@ -1,32 +1,42 @@
 import logging
 from pyhackers.db import DB as db
 from pyhackers.idgen import idgen_client
+from pyhackers.model.cassandra.hierachy import Post
 from pyhackers.model.message import Message
 from pyhackers.events import Event
 
 
-def new_post(message, code=None, current_user_id=None, post_id=None, user_nick=None):
+def new_post(text, code=None, current_user_id=None, post_id=None, user_nick=None):
     logging.warn("Post is=>{}".format(post_id))
-    m = Message()
-    m.id = post_id  #or idgen_client.get()
-    m.user_id = current_user_id
-    m.user_nick = user_nick
-    m.content = message
-    m.content_html = code
 
-    db.session.add(m)
-    success = False
 
-    try:
-        db.session.commit()
-        success = True
-    except Exception, ex:
-        logging.error(ex)
+    message = Post()
+    message.id = post_id or idgen_client.get()
+    message.text = text
+    message.user_id = current_user_id
+    message.save()
+    Event.message(current_user_id, message.id, None)
 
-    if success:
-        Event.message(current_user_id, m.id, None)
+    #m = Message()
+    #m.id = post_id  #or idgen_client.get()
+    #m.user_id = current_user_id
+    #m.user_nick = user_nick
+    #m.content = message
+    #m.content_html = code
+    #
+    #db.session.add(m)
+    #success = False
+    #
+    #try:
+    #    db.session.commit()
+    #    success = True
+    #except Exception, ex:
+    #    logging.error(ex)
 
-    else:
-        logging.warn("Misery sinks in...")
+    #if success:
+
+
+    #else:
+    #    logging.warn("Misery sinks in...")
 
         # Push the post to all of the User's Followers
