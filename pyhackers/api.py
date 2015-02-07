b012a534ef8fdfa669f2ebbea0f758ecff7f26a6
Bahadir Cambel <bcambel@gmail.com>
2015-02-07 16:19:36 +0100

    delete oldies

--- pyhackers/api.py
+++
@@ -1,6 +0,0 @@
-import json
-import logging
-from flask import jsonify, request
-import flask.ext.restless
-from flask.ext.restless.views import ProcessingException
-
--- pyhackers/job_scheduler.py
+++
@@ -1,7 +0,0 @@
-from rq import Queue, Connection
-
-__author__ = 'bahadircambel'
-
-
-with Connection():
-    worker_queue = Queue()
